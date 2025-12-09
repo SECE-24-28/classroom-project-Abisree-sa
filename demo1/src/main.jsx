d4ae1079df55d46b5ec3b7d57eac8b9a234afd2a
unknown <abisreethangaraj@gmail.com>
2025-12-09 14:38:28 +0530

    React

--- demo1/src/main.jsx
+++ demo1/src/main.jsx
@@ -4,16 +4,17 @@ import './index.css'
 import App from './App.jsx'
 import Display from './Display.jsx'
 import Display1 from './Display1.jsx'
+import Display2 from './Display2.jsx'
+import Display4 from './Display4.jsx' 
+import Display5 from './Display5.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {
-      <App />
-      // <Display1/>
-
-       }
-  
-
-
+    {/* <Display1/> */}
+    {/* <App />  */}
+    {/* <Display/> */}
+    {/* <Display2/> */}
+   {/* <Display4/> */}
+   <Display5/>
   </StrictMode>,
 )
